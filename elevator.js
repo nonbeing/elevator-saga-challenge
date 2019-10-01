74ca5d3d5ec2cad17b41012a0e3e69b5442d5256
nonbeing <anattaspace@gmail.com>
2019-10-01 19:35:47 +0530

    Solved challenge 3: 23 in 60s

--- elevator.js
+++ elevator.js
@@ -4,6 +4,20 @@
         var NUM_ELEVATORS = elevators.length;
         var NUM_FLOORS = floors.length - 1;
 
+        function remove_duplicates_es6(arr) {
+            let s = new Set(arr);
+            let it = s.values();
+            return Array.from(it);
+        }
+
+        function remove_floor(arr, floor) {
+            for(var i = arr.length - 1; i >= 0; i--) {
+                if(arr[i] === floor) {
+                   arr.splice(i, 1);
+                }
+            }
+        }
+
         function sortDQ(dq, order) {
             // console.log("[sortDQ] Current DQ before sorting in '" + order + "': " + dq);
             if(order == "ascending"){
@@ -15,21 +29,23 @@
             else {
                 console.log("[sortDQ] ERROR IN sortDQ: improper order!");
             }
-            console.log("[sortDQ] DQ after sorting in '" + order + "': " + dq);
+            // console.log("[sortDQ] DQ after sorting in '" + order + "': " + dq);
+            dq = remove_duplicates_es6(dq);
+            console.log("[sortDQ] DQ after sorting and de-duplicating: " + dq);
             elevator.destinationQueue = dq;
             elevator.checkDestinationQueue(); // enforce new DQ
-            //console.log("[sortDQ] Elevator DQ after sorting in '" + order + "': " + elevator.destinationQueue);
+            //console.log("[sortDQ] DQ after sorting in '" + order + "': " + elevator.destinationQueue);
         }
 
         function decideSort() {
             // console.log("destinationDirection: " + elevator.destinationDirection());
 
             if(elevator.destinationDirection() == "up") {
-                console.log("[decideSort] Elevator going UP! Sorting DQ asc");
+                console.log("[decideSort] Elevator going up, hence sorting DQ asc");
                 sortDQ(elevator.destinationQueue, "ascending");
             }
             else if(elevator.destinationDirection() == "down") {
-                console.log("[decideSort] Elevator going DOWN! Sorting DQ desc");
+                console.log("[decideSort] Elevator going down, hence sorting DQ desc");
                 sortDQ(elevator.destinationQueue, "descending");
             }
             else if(elevator.destinationDirection() == "stopped") {
@@ -51,31 +67,35 @@
         floors.forEach(function(floor) {
             //console.log("working on floor: " + floor);
             floor.on("up_button_pressed", function(floor) {
-                console.log("Floor's UP button pressed on floor: " + floor.floorNum());
+                console.log("[FLOOR_BUTTON] Floor's UP button pressed on floor: " + floor.floorNum());
                 if(elevator.loadFactor() > 0.75) {
                     // Ignore impatient floor button presses
                     // since the elevator is near-full, just go on as usual
-                    console.log("Load Factor > 0.8! Ignore Floor Request!");
-                    console.log("Elevator DQ: " + elevator.destinationQueue);
+                    console.log("[FLOOR_BUTTON] Load Factor HIGH! Ignore Floor Request!");
+                    // console.log("[FLOOR_BUTTON] DQ: " + elevator.destinationQueue);
                 }
                 else {
                     elevator.goToFloor(floor.floorNum()); // enqueue the floor
                     // decideSort();
-                    console.log("Elevator DQ: " + elevator.destinationQueue);
+                    elevator.destinationQueue = remove_duplicates_es6(elevator.destinationQueue);
+                    console.log("[FLOOR_BUTTON] DQ after de-duplicating: " + elevator.destinationQueue);
+                    elevator.checkDestinationQueue(); // enforce new DQ
                 }
             });
             floor.on("down_button_pressed", function(floor) {
-                console.log("Floor's DOWN button pressed on floor: " + floor.floorNum());
+                console.log("[FLOOR_BUTTON] Floor's DOWN button pressed on floor: " + floor.floorNum());
                 if(elevator.loadFactor() > 0.75) {
                     // Ignore impatient floor button presses
                     // since the elevator is near-full, just go on as usual
-                    console.log("Load Factor > 0.8! Ignore Floor Request!");
-                    console.log("Elevator DQ: " + elevator.destinationQueue);
+                    console.log("[FLOOR_BUTTON] Load Factor HIGH! Ignore Floor Request!");
+                    // console.log("[FLOOR_BUTTON] DQ: " + elevator.destinationQueue);
                 }
                 else {
                     elevator.goToFloor(floor.floorNum()); // enqueue the floor
                     // decideSort();
-                    console.log("Elevator DQ: " + elevator.destinationQueue);
+                    elevator.destinationQueue = remove_duplicates_es6(elevator.destinationQueue);
+                    console.log("[FLOOR_BUTTON] DQ after de-duplicating: " + elevator.destinationQueue);
+                    elevator.checkDestinationQueue(); // enforce new DQ
                 }
             });
         });
@@ -90,6 +110,28 @@
             decideSort();
             console.log("Elevator Button Pressed: " + floorNum + ". Current DQ: " + elevator.destinationQueue);
         });
+
+        elevator.on("passing_floor", function(floorNum, direction) {
+             if(elevator.loadFactor() < 0.7) { // if there's still space in the elevator
+                console.log("[PASSING] passing floor: " + floorNum + " with acceptable loadFactor");
+
+                var passing_floor = floors[floorNum];
+
+                if(elevator.destinationQueue.includes(floorNum)) { // if someone's waiting on the passing floor
+                    console.log("[PASSING] STOP at passing floor: " + floorNum);
+                    elevator.goToFloor(floorNum, true); // Do it before anything else
+                    console.log("[PASSING] DQ is now: " + elevator.destinationQueue);
+                }
+             }
+        });
+
+        elevator.on("stopped_at_floor", function(floorNum) {
+            // remove all dupes of this floor from the dq
+            remove_floor(elevator.destinationQueue, floorNum);
+            console.log("[STOPPED] DQ remove all entries of floor '" + floorNum + "' : " + elevator.destinationQueue);
+            // this is okay, because even if some people on this floor can't fit in the elevator,
+            // they will just keep calling the elevator again and again later, so the floor will get enqueued then
+        })
     },
         update: function(dt, elevators, floors) {
             // We normally don't need to do anything here
