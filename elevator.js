a900c49b3676b0ede218e59a195c0d094cd69f3a
nonbeing <anattaspace@gmail.com>
2019-10-05 19:23:50 +0530

    Remove unnecessary sortDQ

--- elevator.js
+++ elevator.js
@@ -3,7 +3,7 @@
         var NUM_ELEVATORS = elevators.length;
         var NUM_FLOORS = floors.length - 1;
         var IDLE_ELEVS = [];
-        const OK_LOAD_FACTOR = 0.8;
+        const OK_LOAD_FACTOR = 0.7;
 
 
         function dedup(arr) {
@@ -22,26 +22,6 @@
         }
 
 
-        // function sortDQ(elevator, order) {
-        //     var dq = elevator.destinationQueue;
-        //     var elev_index = elevators.indexOf(elevator);
-
-        //     if(order == "ascending"){
-        //         dq.sort(function(a, b){return a-b});
-        //     }
-        //     else if (order == "descending") {
-        //         dq.sort(function(a, b){return b-a});
-        //     }
-        //     else {
-        //         console.log("[sortDQ] ERROR IN sortDQ: improper order!");
-        //     }
-        //     // console.log("[sortDQ] DQ after sorting in '" + order + "': " + dq);
-        //     dq = dedup(dq);
-        //     console.log(`[sortDQ] Elev ${elev_index} DQ after '${order}' sort and de-duplicating: ${dq}`);
-        //     elevator.destinationQueue = dq;
-        //     elevator.checkDestinationQueue(); // enforce new DQ
-        // }
-
         function cleanUpDQ(elevator) {
             var elev_index = elevators.indexOf(elevator);
             var dq = elevator.destinationQueue;
@@ -55,7 +35,7 @@
                     var pressedFloors = elevator.getPressedFloors();
 
                     if(!pressedFloors.includes(floorNum)) {// only remove a floorNum if it's not a number pressed inside the elevator
-                        console.log(`[cleanUpDQ ELEV ${elev_index}] Remove floor ${floorNum}`);
+                        console.log(`       [cleanUpDQ ELEV ${elev_index}] Remove floor ${floorNum}`);
                         remove_item_from_arr(dq, floorNum);
                     }
                 }
@@ -82,13 +62,11 @@
 
                 if(closestFloor - curFloor > 0) { // closestFloor is above currentFloor, going UP
                     console.log(`[nDAS:Elev${elev_index}] stopped at:${curFloor}. Going UP to pressedFloor:${closestFloor}`);
-                    // sortDQ(elevator, "ascending");
                     elevator.goingDownIndicator(false);
                     elevator.goingUpIndicator(true);
                 }
                 else {
                     console.log(`[nDAS:Elev${elev_index}] stopped at:${curFloor}. Going DOWN to pressedFloor:${closestFloor}`);
-                    // sortDQ(elevator, "descending");
                     elevator.goingDownIndicator(true);
                     elevator.goingUpIndicator(false);
                 }
@@ -99,13 +77,11 @@
                 var nextFloorNum = elevator.destinationQueue[0];
                 if(nextFloorNum - curFloor > 0) { // nextFloorNum is above currentFloor, going UP
                     console.log(`[nDAS:Elev${elev_index}] stopped at:${curFloor}. Going UP from DQ to:${nextFloorNum}`);
-                    // sortDQ(elevator, "ascending");
                     elevator.goingDownIndicator(false);
                     elevator.goingUpIndicator(true);
                 }
                 else {
                     console.log(`[nDAS:Elev${elev_index}] stopped at:${curFloor}. Going DOWN from DQ to:${nextFloorNum}`);
-                    // sortDQ(elevator, "descending");
                     elevator.goingDownIndicator(true);
                     elevator.goingUpIndicator(false);
                 }
@@ -131,13 +107,11 @@
             // console.log("destinationDirection: " + elevator.destinationDirection());
             if(elevator.destinationDirection() == "up") {
                 console.log(`[directionAndSort] Elev ${elev_index} going up, hence sorting DQ asc`);
-                // sortDQ(elevator, "ascending");
                 elevator.goingDownIndicator(false);
                 elevator.goingUpIndicator(true);
             }
             else if(elevator.destinationDirection() == "down") {
                 console.log(`[directionAndSort] Elev ${elev_index} going down, hence sorting DQ desc`);
-                // sortDQ(elevator, "descending");
                 elevator.goingDownIndicator(true);
                 elevator.goingUpIndicator(false);
             }
@@ -313,6 +287,8 @@
            // elevator was traveling and has arrived at floor
            elevator.on("stopped_at_floor", function(floorNum) {
                // remove all dupes of this floor from the dq
+               // this is okay, because even if some people on this floor can't fit in the elevator,
+               // they will just keep calling the elevator again and again later, so the floor will get enqueued then
                remove_item_from_arr(elevator.destinationQueue, floorNum);
                console.log(`[ELEV ${this_elev_index} STOPPED at ${floorNum}] removed all entries of floor ${floorNum}. DQ: '${elevator.destinationQueue}'`);
 
@@ -320,8 +296,6 @@
                cleanUpDQ(elevator);
 
                newDirectionAfterStopping(elevator);
-               // this is okay, because even if some people on this floor can't fit in the elevator,
-               // they will just keep calling the elevator again and again later, so the floor will get enqueued then
            });
         });
     },
